3d2e7deb79d39934445ae48e8807999c97ba160b
ckirschberg@gmail.com <ckirschberg@gmail.com>
2019-02-07 15:24:57 +0100

    started reactive forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { RegisterComponent } from './register/register.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login', // name of component
@@ -6,10 +7,31 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) {
+  }
 
   ngOnInit() {
+    this.loginForm = this.fb.group(
+      {
+        username: ['', [Validators.required, Validators.minLength(3)]], // multiple validators
+        password: ['', Validators.required] // Single validator
+      }
+    )
   }
+
+  onSubmit() : void {
+    console.log(this.loginForm);
+
+    if (this.loginForm.valid) {
+      // Send the data to the server to verify the user login
+    }
+    else {
+      // Show error message or something else.
+    }
+
+  }
+
 
 }
